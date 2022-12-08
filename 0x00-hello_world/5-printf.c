f2ea5fc9f7463596f3a37f5a3142e3c4cfeed448
Gabryelcode <gabrieledeomam@gmail.com>
2022-12-08 13:40:50 -0800

    corrected

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 
 /**
- *Description: Main function
+ * int main - Short description
+ * integer function
+ * void: Argument
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 /**
- *Short Description: Returns integer zero
+ * return - Short description
+ * (0): Returns zero intoger
  */
 	return (0);
 }
