61b42f087014cca293bf87d8f98c3f14a6e88a0c
Gabryelcode <gabrieledeomam@gmail.com>
2022-12-08 13:47:14 -0800

    corrected

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,7 +2,7 @@
 
 /**
  * int main - Short description
- * integer function
+ * main: This is a function where all the commands and compilation starts from
  * void: Argument
  */
 int main(void)
@@ -10,7 +10,7 @@ int main(void)
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 /**
  * return - Short description
- * (0): Returns zero intoger
+ * return value: Returns zero integer to main for the end of the compilation
  */
 	return (0);
 }
