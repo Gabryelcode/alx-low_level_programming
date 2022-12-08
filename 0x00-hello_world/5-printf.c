1ce8acf73434899ecaab6a014767ea90cd1c1093
Gabryelcode <gabrieledeomam@gmail.com>
2022-12-08 15:50:29 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,14 +2,11 @@
 /**
  * main - Prints the phrase with proper grammar, 
  * but the outcome is a piece of art,
- * return: 0 if exited properly, non-zero otgerwise
+ *
+ * Return: Always 0 (Succes)
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-/**
- * return - Short description
- * return - 0 if exited properly, non-zero otherwise
- */
 	return (0);
 }
