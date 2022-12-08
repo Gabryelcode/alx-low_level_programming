662ddbfda15d2e4e06275058e4db6444db2ef2a7
Gabryelcode <gabrieledeomam@gmail.com>
2022-12-08 14:48:38 -0800

    comment

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-
 /**
  * main - Prints the phrase with proper grammar, 
- * but the outcome is a piece of art, 
+ * but the outcome is a piece of art,
+ * return: 0 if exited properly
  */
 int main(void)
 {
