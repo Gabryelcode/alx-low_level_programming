1be4c43a3ce031360873abe92869d4c45f6d7d4d
Gabryelcode <gabrieledeomam@gmail.com>
2022-12-08 15:19:22 -0800

    C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,12 @@
  * main - prints the phrase "Programming is like building a multilingual puzzle"
  * return: 0 if exited properly, otherwise non-zero 
  */
+
 int main(void)
 {
-	puts("\"Programmimg is like building a  multilingual puzzle");
+	puts("\"Programmimg is like building a multilingual puzzle");
+/**
+ * return - 0 integer if exited properly, otherwise a non zero
+ */
 	return (0);
 }
