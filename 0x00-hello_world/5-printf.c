bde28fa4985d5b79aff45b3a6dd5a3650b80f420
Gabryelcode <gabrieledeomam@gmail.com>
2022-12-08 14:44:14 -0800

    corrected

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- * int main - Short description
- * main function: Prints the clause with proper grammar, 
- * but the outcome is a piece of art,
- * return: 0 if exited properly, non-zero otherwise 
+ * main - Prints the phrase with proper grammar, 
+ * but the outcome is a piece of art, 
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-
+/**
+ * return - 0 if exited properly, non-zero otherwise
+ */
 	return (0);
 }
