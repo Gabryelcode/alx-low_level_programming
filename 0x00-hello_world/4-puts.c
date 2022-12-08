dbf23703b8027de8c549f912fa5c54c09c929138
Gabryelcode <gabrieledeomam@gmail.com>
2022-12-08 15:28:32 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -10,6 +10,7 @@ int main(void)
 	puts("\"Programmimg is like building a multilingual puzzle");
 /**
  * return - 0 integer if exited properly, otherwise a non zero
+ * 0: integer that is returned to main
  */
 	return (0);
 }
