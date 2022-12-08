af19a2f60d58908af61d18647a1931c233997718
Gabryelcode <gabrieledeomam@gmail.com>
2022-12-08 13:54:39 -0800

    Corrected

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
-
+7
 /**
  * int main - Short description
- * int main: This is a function where all the commands and compilation starts from
- * void: Argument
+ * main: This is a function where all the commands and compilation starts from
+ * @void: Argument
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 /**
  * return - Short description
- * return (0): Returns zero integer to main for the end of the compilation
+ * @0: Returns zero integer to main for the end of the compilation
  */
 	return (0);
 }
