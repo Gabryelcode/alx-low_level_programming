d4154df4d922b12bfde0fb5b16b1d0be2f19cb9f
Gabryelcode <gabrieledeomam@gmail.com>
2022-12-08 13:30:22 -0800

    Program with Description embedded

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
 /**
-  *Main function
-*/
+ *Description: Main function
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-
+/**
+ *Short Description: Returns integer zero
+ */
 	return (0);
 }
