4309967e2312016b112d451680b1127719773afc
Gabryelcode <gabrieledeomam@gmail.com>
2022-12-08 15:43:46 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,8 @@
 
 /**
  * main - prints the phrase "Programming is like building a multilingual puzzle"
- * return: 0 if exited properly, otherwise non-zero 
+ * 
+ * Return: Always 0 (success) 
  */
 
 int main(void)
