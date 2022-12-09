a2ad8aad85a4788a505ef8d02c52b250918801a7
Gabryelcode <gabrieledeomam@gmail.com>
2022-12-08 16:42:33 -0800

    grammar

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - Prints the phrase with proper grammar, 
+ * main - prints the phrase with proper grammer,
  * but the outcome is a piece of art,
  *
  * Return: Always 0 (Succes)
