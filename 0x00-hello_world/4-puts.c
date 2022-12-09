1d1fed42592c0c69ffb10dd118c08f406ad6cab6
Gabryelcode <gabrieledeomam@gmail.com>
2022-12-08 16:57:03 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-
 /**
- * main - prints the phrase "Programming is like building a multilingual puzzle"
- * 
- * Return: Always 0 (success) 
+ * main - prints "Progimg is like  building a multilingual puzzle
+ *
+ * Return: Always 0  (Success)
  */
-
 int main(void)
 {
-	puts("Programmimg is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
