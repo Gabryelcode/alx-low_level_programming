ff681f784d5c2e00afc49d6d50dea663c7729f76
Gabryelcode <gabrieledeomam@gmail.com>
2022-12-12 09:26:31 -0800

    Corrected

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - prints the entry result
- * 
+ * main - prints and that piece of art is useful -
+ * Dora Korpar, 2015-10-19
+ * folowed by a new line
+ *
  * Returns: Always 0 (Success)
  */
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
 	return (1);
 }
