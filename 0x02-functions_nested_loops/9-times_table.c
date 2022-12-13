7c4cff2f10459a2b109493f2bcf7115ebf2ccd5c
Gabryelcode <gabrieledeomam@gmail.com>
2022-12-13 14:50:43 -0800

    Creating a function that prints the 9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,14 +15,12 @@ void times_table(void)
 
 			if (j == 0)
 			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
 				_putchar(k + '0');
 			}
 			if (k < 10 && j != 0)
 			{
 				_putchar(',');
+				_putchar(' ');
 				_putchar(' ');
 				_putchar(k + '0');
 			}
