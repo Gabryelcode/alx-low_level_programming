6f2dd646ee1f8aace73597c2da6da3cf27b8c0ff
Gabryelcode <gabrieledeomam@gmail.com>
2022-12-13 17:35:27 -0800

    Creating a function that prints the times table of any integer n except greater than 15 and less than 0

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * print_times_table - prints the times table of an integer n
+ * @n: receives the integer for multiplication
+ *
+ */
+void print_times_table(int n)
+{
+	int a, b, c;
+
+	if (n <= 15 && n > 0)
+	{
+		for (a = 0; a <= n; a++)
+		{
+			for (b = 0; b <= n; b++)
+			{
+				c = a * b;
+				if (b == 0)
+				{
+					printf("%d", c);
+				}
+				if (c >= n || b != 0)
+				{
+					printf(",");
+					printf(" ");
+					printf("%d", c);
+				}
+
+			}
+		}
+}
