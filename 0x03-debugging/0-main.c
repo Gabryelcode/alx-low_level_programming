79870bd602a6534d651ce4d9bc12fba0fedce218
Gabryelcode <gabrieledeomam@gmail.com>
2022-12-14 03:30:51 -0800

    Creating a test program that debugs

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - prints positive or negative number
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	positive_or_negative(n);
+
+	return (0);
+}
