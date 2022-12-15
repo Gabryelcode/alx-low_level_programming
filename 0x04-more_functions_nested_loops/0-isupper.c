916032a8551c240710a7c1f5a060d6346ed4c326
Gabryelcode <gabrieledeomam@gmail.com>
2022-12-15 02:10:31 -0800

    Creating a function that returns 1 for uppercase letters and 0 otherwise

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper - checks if letter is uppercase
+ * @c: receives the character
+ *
+ * Return: 1 if letter is uppercase and 0 otherwise
+ */
+int _isupper(int c)
+{
+	if (c <= 'Z')
+	{
+		return (1);
+	}
+	else
+
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void _putchar(int x);
+int _isupper(int c);
+
+#endif
