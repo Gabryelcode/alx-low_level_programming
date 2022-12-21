08f6d3810680ab19f2560ed1c3e6a7ada931e1cb
Gabryelcode <gabrieledeomam@gmail.com>
2022-12-21 14:15:21 -0800

    Creating a function that compares two strings and returns a value if condition is met

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+/**
+ * _strcmp - compares two strings and returns a value
+ * @s1: receives the first string
+ * @s2: receives the second string
+ *
+ * Return: an integer if condition is met
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int a, b;
+
+	for (a = 0; s1[a] != '\0'; a++)
+		;
+	for (b = 0; s2[b] != '\0'; b++)
+		;
+	if (a < b)
+		return (-15);
+	else if (a > b)
+		return (15);
+	else
+		return (0);
+}
