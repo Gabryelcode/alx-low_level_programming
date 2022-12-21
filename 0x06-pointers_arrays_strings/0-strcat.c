c4c164a6b39751599f1fece8daecd2755ca3c0dc
Gabryelcode <gabrieledeomam@gmail.com>
2022-12-21 10:22:33 -0800

    Creating a function that concatenates a string in another and returning a pointer

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strcat - concatenantes a string in another
+ * @dest: receives the first string
+ * @src: receives the second string
+ *
+ * Return: A pointer
+ */
+char *_strcat(char *dest, char *src)
+{
+    int a, b, c, n;
+
+    for (a = 0; dest[a]; a++)
+        ;
+    for (b = 0; src[b]; b++)
+        ;
+
+    for (c = 0; c < b; c++)
+    {
+        n = a++;
+        dest[n] = src[c];
+    }
+    return (dest);
+}
