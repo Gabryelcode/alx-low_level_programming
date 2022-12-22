d2798a9821eba40f87eaeff7621285708b6ee18d
Gabryelcode <gabrieledeomam@gmail.com>
2022-12-21 16:18:48 -0800

    Creating a function that reverses an array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -6,15 +6,22 @@
  */
 void reverse_array(int *a, int n)
 {
-	int i;
+	int temp, s, e;
 
-	i = n - 1;
-	while (i < n && i >= 0)
+	s = 0;
+	e = n - 1;
+	/**
+	 * set value in array a in temp
+	 * then place the last array in
+	 * the first array then place
+	 * value in temp to last array
+	*/
+	while (s < e)
 	{
-		printf("%d", a[i]);
-		if (i != 0)
-			printf(", ");
-		i--;
+		temp = a[s];
+		a[s] = a[e];
+		a[e] = temp;
+		s++;
+		e--;
 	}
-	printf("\n");
 }
