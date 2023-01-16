55a91fae1a143cae235696f8297f9b1d45bceb4a
Gabryelcode <gabrieledeomam@gmail.com>
2023-01-16 11:48:44 -0800

    initializing dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,12 @@
 #include "dog.h"
 
+/**
+ * init_dog - initializing dog
+ * @d: pointer to struct dog
+ * @name: pointer to name
+ * @age: receives age
+ * @owner: pointer to owner
+ */
 void init_dog(struct dog *d, char *name, float *age, char *owner)
 {
 	if (d)
