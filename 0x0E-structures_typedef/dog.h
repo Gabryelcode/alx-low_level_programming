2e22505de038f7bff44cdfd66944bdc8483b8aa5
Gabryelcode <gabrieledeomam@gmail.com>
2023-01-16 11:40:56 -0800

    initializing dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
+
 /**
  * struct dog - new type struct for dog
  * @name: receives the dog's name
@@ -12,5 +13,7 @@ struct dog
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float *age, char *owner);
 
 #endif
