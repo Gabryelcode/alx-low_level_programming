0d188941781e813b72b4d351edb891c6750303c7
Gabryelcode <gabrieledeomam@gmail.com>
2023-01-16 11:07:30 -0800

    Struct for dogs info

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - new type struct for dog
+ * @name: receives the dog's name
+ * @age: receives the dog's age
+ * @owner: receives the owner's name
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
