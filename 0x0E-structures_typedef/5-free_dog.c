84c11c30616cfaa6b6d19d5821215fd3684dfc2e
Gabryelcode <gabrieledeomam@gmail.com>
2023-01-17 07:08:44 -0800

    Freeing dog

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -2,10 +2,14 @@
 #include <stdio.h>
 /**
  * free_dog - initializing dog
+ *
  * @d: pointer to free dog
+ *
  */
 void free_dog(dog_t *d)
 {
+	d = malloc(sizeof(dog_t));
+
 	if (d)
 	{
 		free(d->name);
